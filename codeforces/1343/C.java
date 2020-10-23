a25805e16b730beae4c5e1e7e22b143bb60490f8
Warul Kumar Sinha <wksinha@outlook.com>
2020-10-24 00:01:00 +0530

    Add solution for problem `C - Alternating Subsequence` Link: https://codeforces.com/contest/1343/problem/C Tags: dp, greedy, two pointers, *1200 Ref: https://codeforces.com/contest/1343/submission/96474700

--- codeforces/1343/C.java
+++ codeforces/1343/C.java
@@ -18,36 +18,83 @@ public class CF1343C {
 
     // Computes and returns sum of longest alternating sequence with maximum sum
     static long getSolutionSum(int[] arr) {
-        Vector<Integer> solution = new Vector<>();
-        boolean prevPos = arr[0] > 0;
-        int appendSolution = arr[0]; // Candidate number to be appended to solution vector
+        Vector<Integer> negFirst = new Vector<>();
+        Vector<Integer> posFirst = new Vector<>();
+        boolean prevPosNF = false; // Notes whether the last addition to negFirst was posititve or not 
+        boolean prevPosPF = true; // Notes whether the last addition to posFirst was positive or not
+
+        int addToNF = Integer.MIN_VALUE; // This won't be added
+        int addToPF = Integer.MIN_VALUE; // This won't be added
+
         for (int i = 0; i < arr.length; ++i) {
             boolean isPos = arr[i] > 0;
-            if (prevPos) {
+            // Working on negFirst vector
+            if (prevPosNF) {
                 if (isPos) {
-                    appendSolution = Math.max(arr[i], appendSolution);
+                    addToNF = Math.max(arr[i], addToNF);
                 }
                 else {
-                    solution.add(appendSolution);
-                    appendSolution = arr[i];
-                    prevPos = false;
+                    negFirst.add(addToNF);
+                    addToNF = arr[i];
+                    prevPosNF = false;
                 }
             }
             else {
                 if (isPos) {
-                    solution.add(appendSolution);
-                    appendSolution = arr[i];
-                    prevPos = true;
+                    if (addToNF != Integer.MIN_VALUE) // Initial value
+                        negFirst.add(addToNF);
+                    addToNF = arr[i];
+                    prevPosNF = true;
                 }
                 else {
-                    appendSolution = Math.max(appendSolution, arr[i]);
+                    addToNF = Math.max(arr[i], addToNF);
+                }
+            }
+            // Working on posFirst vector
+            if (prevPosPF) {
+                if (isPos) {
+                    addToPF = Math.max(arr[i], addToPF);
+                }
+                else {
+                    if (addToPF != Integer.MIN_VALUE)
+                        posFirst.add(addToPF);
+                    addToPF = arr[i];
+                    prevPosPF = false;
+                }
+            }
+            else {
+                if (isPos) {
+                    posFirst.add(addToPF);
+                    addToPF = arr[i];
+                    prevPosPF = true;
+                }
+                else {
+                    addToPF = Math.max(arr[i], addToPF);
                 }
             }
         }
-        //Adding last element
-        solution.add(appendSolution);
+        // Adding last element if not added already
+        if (negFirst.size() == 0 && addToNF != Integer.MIN_VALUE)
+            negFirst.add(addToNF);
+        else {
+            int lastAddedNF = negFirst.get(negFirst.size()-1);
+            if (lastAddedNF != addToNF)
+                negFirst.add(addToNF);
+        }
+        if (posFirst.size() == 0 && addToPF != Integer.MIN_VALUE)
+            posFirst.add(addToPF);
+        else {
+            int lastAddedPF = posFirst.get(posFirst.size()-1);
+            if (lastAddedPF != addToPF)
+                posFirst.add(addToPF);
+        }
         
-        return vectorSum(solution);
+        if (negFirst.size() > posFirst.size())
+            return vectorSum(negFirst);
+        else if (posFirst.size() > negFirst.size())
+            return vectorSum(posFirst);
+        else // Same size
+            return Math.max(vectorSum(negFirst), vectorSum(posFirst));
     }
 
     // Returns sum of elements of vector
