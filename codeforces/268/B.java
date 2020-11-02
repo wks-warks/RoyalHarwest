8f98f2793479442f7dd4a44aac653a084690b847
Warul Kumar Sinha <wksinha@outlook.com>
2020-11-02 11:43:00 +0530

    Add solution for problem `B - Buttons` Link: https://codeforces.com/contest/268/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/268/submission/97401778

--- codeforces/268/B.java
+++ codeforces/268/B.java
@@ -12,26 +12,10 @@ public class CF268B {
     // Computes and returns number of moves required in worst case
     static int computeMovesRequired(int buttons) {
         int movesRequired = 0;
-        // Direct Method
-        return (int)(((long)buttons*buttons*buttons+5*buttons)/6);
-    }
-}
-
-// Sequential method
-/* 
-        int movesRequired = 0;
         for (int sequenceFound = 0; sequenceFound < buttons; ++sequenceFound) {
             int sequenceRemaining = buttons - sequenceFound;
             movesRequired += 1 + (sequenceFound+1) * (sequenceRemaining-1);
         }
         return movesRequired;
-*/
-
-/*
-sequenceRemaining = buttons
-    Sigma (1 + i*(n-i)) over 1 to n
-    = n + n*Sigma(i) - Sigma(i*i)
-    = n + n*(n)*(n+1)/2 - (n)(n+1)(2n+1)/6
-    = 6n + 3n3 +3n2 - 2n3-3n2-n
-    = (n3 +5n)/6
-*/
+    }
+}
