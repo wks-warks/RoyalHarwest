77ed5f21b7d2e4fa39be4bea45056f5a232707b4
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-06 20:51:00 +0530

    Add solution for problem `B - Balls of Steel` Link: https://codeforces.com/contest/1450/problem/B Tags: brute force, geometry, greedy, *1000 Ref: https://codeforces.com/contest/1450/submission/100550986

--- codeforces/1450/B.java
+++ codeforces/1450/B.java
@@ -1,6 +1,6 @@
 //Codeforces 1450 B 
 import java.util.*;
- 
+
 public class CFB {
     static final Scanner SC = new Scanner(System.in);
     public static void main(String[] args) {
@@ -19,44 +19,23 @@ public class CFB {
             System.out.println(minOperations);
         }
     }
- 
+
     // Computes and returns minimum number of operations required to get all balls at the same pos
     static int countMinOperations(int attractPower, Pair[] positions, int balls) {
-        int[] distance = new int[balls];
-        int minOperations = Integer.MAX_VALUE;
         for (int ballConsidered = 0; ballConsidered < balls; ++ballConsidered) {
-            for (int i = 0; i < distance.length; ++i)
-                distance[i] = Integer.MAX_VALUE;
-            distance[ballConsidered] = 0;
-            boolean[] isVisited = new boolean[balls];
-            isVisited[ballConsidered] = true;
-            Queue<Integer> nodeList = new PriorityQueue<Integer>();
-            nodeList.add(ballConsidered);
-            while (nodeList.size() > 0) {
-                int idx = nodeList.poll();
-                Pair presentPos = positions[idx];
-                int presDistance = distance[idx];
-                for (int i = 0; i < balls; ++i) {
-                    if (isVisited[i])
-                        continue;
-                    if (Math.abs(positions[i].x - presentPos.x) + Math.abs(positions[i].y - presentPos.y) <= attractPower) {
-                        distance[i] = presDistance + 1;
-                        nodeList.add(i);
-                        isVisited[i] = true;
-                    }
+            int reachable = 0;
+            for (int b = 0; b < balls; ++b) {
+                if (Math.abs(positions[b].x - positions[ballConsidered].x) + Math.abs(positions[b].y - positions[ballConsidered].y) <= attractPower) {
+                    reachable += 1;
                 }
             }
-            
-            int maxDistance = -1;
-            for (int i = 0; i < distance.length; ++i) {
-                maxDistance = Math.max(maxDistance, distance[i]);
-            }
-            minOperations = maxDistance != Integer.MAX_VALUE ? Math.min(minOperations, maxDistance) : minOperations;
+            if (reachable == balls)
+                return 1;
         }
-        return minOperations == 1 ? minOperations : -1;
+        return -1;
     }
 }
- 
+
 class Pair {
     int x, y;
     public Pair(int x, int y) {
