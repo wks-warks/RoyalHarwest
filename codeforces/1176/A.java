2c0c6f149e9245b64644e9d1d4639dc81a1fa597
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-08 06:48:00 +0530

    Add solution for problem `A - Divide it!` Link: https://codeforces.com/contest/1176/problem/A Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1176/submission/100661173

--- codeforces/1176/A.java
+++ codeforces/1176/A.java
@@ -1,10 +1,11 @@
-//Codeforces 1176Adp 
+//Codeforces 1176A 
 import java.util.Scanner;
 import java.util.HashMap;
 
-public class CF1176Adp {
+public class CF1176A {
     static final Scanner SC = new Scanner(System.in);
-    // DP Solution
+/*    
+    // DP Solution - TLE
     static HashMap<Long, Integer> numberAndMoves;
     public static void main(String[] args) {
         numberAndMoves = new HashMap<Long, Integer>();
@@ -30,15 +31,47 @@ public class CF1176Adp {
             if (computeRequiredMoves(n/2) != -1)
                 requiredMoves = 1 + computeRequiredMoves(n/2);
         }
-        else if (n % 3 == 0) {
+        if (n % 3 == 0) {
             if (computeRequiredMoves(2*n/3) != -1)
-                requiredMoves = 1 + computeRequiredMoves(2*n/3);
+                requiredMoves = Math.min(requiredMoves, 1 + computeRequiredMoves(2*n/3));
         }
-        else if (n % 5 == 0) {
+        if (n % 5 == 0) {
             if (computeRequiredMoves(4*n/5) != -1)
-                requiredMoves = 1 + computeRequiredMoves(4*n/5);
+                requiredMoves = Math.min(requiredMoves, 1 + computeRequiredMoves(4*n/5));
         }
         numberAndMoves.put(n, requiredMoves != Integer.MAX_VALUE ? requiredMoves : -1);
         return numberAndMoves.get(n);            
     } 
+*/
+    public static void main(String[] args) {
+        int queries = SC.nextInt();
+        for (int q = 0; q < queries; ++q) {
+            long n = SC.nextLong();
+            int requiredMoves = computeRequiredMoves(n);
+            System.out.println(requiredMoves);
+        }
+    }
+
+    // Computes and returns minimum number of moves to get number to 1
+    static int computeRequiredMoves(long n) {
+        int moves = 0;
+        while (n % 5 == 0) {
+            moves += 1;
+            n *= 4;
+            n /= 5;
+        }
+        while (n % 3 == 0) {
+            moves += 1;
+            n *= 2;
+            n /= 3;
+        }
+        while (n % 2 == 0) {
+            moves += 1;
+            n /= 2;
+        }
+        if (n != 1)
+            return -1;
+        else
+            return moves;
+    }
 }
