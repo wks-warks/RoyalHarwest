d3ee943cbd893f731ae8047c736f9531c29488f7
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-10 15:55:00 +0530

    Add solution for problem `C - k-Tree` Link: https://codeforces.com/contest/431/problem/C Tags: dp, implementation, trees, *1600 Ref: https://codeforces.com/contest/431/submission/100824665

--- codeforces/431/C.java
+++ codeforces/431/C.java
@@ -11,7 +11,6 @@ public class CF431C {
         System.out.println(ways);
     }
 
-/*
     static int[] dpAtLeastD;
     static int[] dp;
     // Computes and returns the answer
@@ -31,26 +30,5 @@ public class CF431C {
             }
         }
         return dpAtLeastD[n];
-    }
-*/
-
-    // Alternatively
-    static int[][] dp;
-    // Computes and returns the answer
-    static int computeWays(int n, int k , int d) {
-        dp = new int[n+1][2]; // 0: no edge of length at least d, 1: found edge of length at least d
-        dp[0][0] = 1;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 1; j <= Math.min(k, n-i); ++j) {
-                dp[i+j][0] += dp[i][0];
-                dp[i+j][0] %= 1_000_000_007;
-                if (j >= d)
-                    dp[i+j][1] += dp[i][0];
-                else
-                    dp[i+j][1] += dp[i][1];
-                dp[i+j][1] %= 1_000_000_007;
-            }
-        }
-        return dp[n][1];
     }
 }
