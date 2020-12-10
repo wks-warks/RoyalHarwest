d48b8a22b44811261126026482d6c89078410d0b
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-10 23:39:00 +0530

    Add solution for problem `A - Angry Students` Link: https://codeforces.com/contest/1287/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1287/submission/100857781

--- codeforces/1287/A.java
+++ codeforces/1287/A.java
@@ -1,37 +1,3 @@
-// //Codeforces 1287A 
-// import java.util.Scanner;
-
-// public class CF1287A {
-//     static final Scanner SC = new Scanner(System.in);
-//     public static void main(String[] args) {
-//         int tests = SC.nextInt();
-//         for (int t = 0; t < tests; ++t) {
-//             int students = SC.nextInt();
-//             String state = SC.next();
-//             int timeUntilArmageddon = computeTimeUntilArmageddon(state);
-//             System.out.println(timeUntilArmageddon);
-//         }
-//     }
-
-//     // Computes and returns time until all students are in the worst possible state that they can be given the initial states
-//     static int computeTimeUntilArmageddon(String state) {
-//         boolean angryFound = false;
-//         int maxTime = 0;
-//         int time = 0;
-//         for (int i = 0; i < state.length(); ++i) {
-//             if (state.charAt(i) == 'A') {
-//                 angryFound = true;
-//                 time = 0;
-//             }
-//             else if (angryFound) {
-//                 time += 1;
-//             }
-//             maxTime = Math.max(time, maxTime);
-//         }
-//         return maxTime;
-//     }
-// }
-
 //Codeforces 1287A 
 import java.util.Scanner;
 
@@ -49,23 +15,19 @@ public class CF1287A {
 
     // Computes and returns time until all students are in the worst possible state that they can be given the initial states
     static int computeTimeUntilArmageddon(String state) {
-        int prevAngry = -1;
+        boolean angryFound = false;
         int maxTime = 0;
+        int time = 0;
         for (int i = 0; i < state.length(); ++i) {
             if (state.charAt(i) == 'A') {
-                if (prevAngry == -1) {
-                    prevAngry = i;
-                    continue;
-                }
-                int time = i - prevAngry - 1;
-                maxTime = Math.max(time, maxTime);
-                // System.out.println("MT" + maxTime);
-                prevAngry = i;
+                angryFound = true;
+                time = 0;
             }
+            else if (angryFound) {
+                time += 1;
+            }
+            maxTime = Math.max(time, maxTime);
         }
-
-        if (prevAngry != -1)
-            maxTime = Math.max(maxTime, state.length() - prevAngry - 1);
         return maxTime;
     }
 }
