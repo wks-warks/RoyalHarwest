d7731f4150025e40a3b6fd70e4ecdc00dd133bb7
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-16 16:30:00 +0530

    Add solution for problem `B - Quizzes` Link: https://atcoder.jp/contests/abc184/tasks/abc184_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc184/submissions/18809225

---
+++ atcoder/abc184/B.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static final FastReader FR = new FastReader();
+
+    public static void main(String[] args) {
+        StringBuilder solution = new StringBuilder();
+        int tests = 1;
+        // tests = FR.nextInt();
+        for (int t = 0; t < tests; ++t) {
+            int questions = FR.nextInt();
+            int initialPoints = FR.nextInt();
+            String results = FR.next();
+            int points = initialPoints;
+            for (char r : results.toCharArray()) {
+                if (r == 'o') {
+                    points += 1;
+                }
+                else {
+                    points = Math.max(0, points-1);
+                }
+            }
+            solution.append(points);
+        }
+        System.out.println(solution.toString());
+    }
+
+    // Fast Input - GFG
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e)  { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+}
