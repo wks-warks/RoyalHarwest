2f598c83e9112c26b64859d8189ff9630e0c32dc
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-23 11:20:00 +0530

    Add solution for problem `A - Iroha and Haiku (ABC Edition)` Link: https://atcoder.jp/contests/abc042/tasks/abc042_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc042/submissions/18948262

---
+++ atcoder/abc042/A.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static final FastReader FR = new FastReader();
+
+    public static void main(String[] args) {
+        StringBuilder solution = new StringBuilder();
+        int tests = 1;
+        // tests = FR.nextInt();
+        for (int t = 0; t < tests; ++t) {
+            Map <Integer, Integer> count = new TreeMap<>();
+            for (int i = 0; i < 3; ++i) {
+                int num = FR.nextInt();
+                if (count.containsKey(num)) {
+                    count.put(num, count.get(num)+1);
+                }
+                else {
+                    count.put(num, 1);
+                }
+            }
+            boolean canForm = count.get(5) == 2 && count.get(7) == 1;
+            String response = canForm ? "YES" : "NO";
+            solution.append(response);
+        }
+        System.out.println(solution.toString());
+    }
+
+    // Fast Input - GFG
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e)  { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+}
