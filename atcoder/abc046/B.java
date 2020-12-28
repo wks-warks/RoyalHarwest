5d205d01711878b7886ebdbb879674c6390e963b
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-28 13:54:00 +0530

    Add solution for problem `B - Painting Balls with AtCoDeer` Link: https://atcoder.jp/contests/abc046/tasks/abc046_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc046/submissions/19027691

---
+++ atcoder/abc046/B.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static final FastReader FR = new FastReader();
+
+    public static void main(String[] args) {
+        StringBuilder solution = new StringBuilder();
+        int tests = 1;
+        // tests = FR.nextInt();
+        for (int t = 0; t < tests; ++t) {
+            int balls = FR.nextInt();
+            int colors = FR.nextInt();
+            int answer = 1;
+            while (balls-->1)
+                answer *= colors-1;
+            answer *= colors;
+            solution.append(answer);
+        }
+        System.out.println(solution.toString());
+    }
+
+    // Fast Input - GFG
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e)  { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+}
