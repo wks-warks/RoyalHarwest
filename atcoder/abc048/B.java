8b3c92949d7f2c30d38e62527e67930e4091cafe
Warul Kumar Sinha <wksinha@outlook.com>
2020-12-30 20:40:00 +0530

    Add solution for problem `B - Between a and b ...` Link: https://atcoder.jp/contests/abc048/tasks/abc048_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc048/submissions/19066895

---
+++ atcoder/abc048/B.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static final FastReader FR = new FastReader();
+
+    public static void main(String[] args) {
+        StringBuilder solution = new StringBuilder();
+        int tests = 1;
+        // tests = FR.nextInt();
+        for (int t = 0; t < tests; ++t) {
+            long a = FR.nextLong();
+            long b = FR.nextLong();
+            long x = FR.nextLong();
+            long answer = getAnswer(a, b, x);
+            solution.append(answer);
+        }
+        System.out.println(solution.toString());
+    }
+
+    // Computes and returns answer
+    static long getAnswer(long a, long b, long x) {
+        long left = a % x == 0 ? a : a - a % x + x; 
+        long right = b - b % x;
+        long answer = right > left ? (right-left) / x + 1 : right == left ? 1 : 0;
+        return answer;
+    }
+
+    // Fast Input - GFG
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e)  { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+}
