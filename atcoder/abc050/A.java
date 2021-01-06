ee6c8ece6ed14166e6fe3e30b1da895879f27f83
Warul Kumar Sinha <wksinha@outlook.com>
2021-01-06 20:08:00 +0530

    Add solution for problem `A - Addition and Subtraction Easy` Link: https://atcoder.jp/contests/abc050/tasks/abc050_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc050/submissions/19237740

---
+++ atcoder/abc050/A.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+ 
+class Main {
+    static final FastReader FR = new FastReader();
+ 
+    public static void main(String[] args) {
+        StringBuilder solution = new StringBuilder();
+        int tests = 1;
+        // tests = FR.nextInt();
+        for (int t = 0; t < tests; ++t) {
+            int a = FR.nextInt();
+            char op = FR.next().charAt(0);
+            int b = FR.nextInt();
+            switch(op) {
+                case '+': solution.append(a+b);
+                break;
+                default: solution.append(a-b);
+            }
+        }
+      	PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out));
+		writer.print(solution.toString());
+        writer.close();
+    }
+ 
+    // Fast Input - GFG
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+ 
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e)  { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+}
