5f888c90c9fa64a72651d2794c100a9081ebf7f9
Warul Kumar Sinha <wksinha@outlook.com>
2021-01-10 17:31:00 +0530

    Add solution for problem `A - Three-Point Shot` Link: https://atcoder.jp/contests/abc188/tasks/abc188_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc188/submissions/19313569

---
+++ atcoder/abc188/A.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+ 
+class Main {
+    static final FastReader FR = new FastReader();
+ 
+    public static void main(String[] args) {
+        StringBuilder solution = new StringBuilder();
+        int tests = 1;
+        // tests = FR.nextInt();
+        for (int t = 0; t < tests; ++t) {
+            int x = FR.nextInt();
+            int y = FR.nextInt();
+            boolean turnTables = (Math.min(x,y) + 3 > Math.max(x, y));
+            solution.append(turnTables? "Yes" : "No");
+        }
+      	PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out));
+		writer.print(solution.toString());
+        writer.close();
+    }
+ 
+    // Fast Input - GFG
+    static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+ 
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } catch (IOException e)  { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+}
