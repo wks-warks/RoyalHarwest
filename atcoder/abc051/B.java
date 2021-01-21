fc7cc38e64bde1da73f2a94a95a81b8053f60a87
Warul Kumar Sinha <wksinha@outlook.com>
2021-01-21 23:51:00 +0530

    Add solution for problem `B - Sum of Three Integers` Link: https://atcoder.jp/contests/abc051/tasks/abc051_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc051/submissions/19556414

---
+++ atcoder/abc051/B.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+  static final FastReader FR = new FastReader();
+  static final PrintWriter PW = new PrintWriter(new OutputStreamWriter(System.out));
+ 
+  public static void main(String[] args) {
+    StringBuilder solution = new StringBuilder();
+    int tests = 1;
+    // tests = FR.nextInt();
+    for (int t = 0; t < tests; ++t) {
+      int max = FR.nextInt();
+      int sum = FR.nextInt();
+      int count = 0;
+      for (int i = 0; i <= max; i += 1) {
+        for (int j = 0; j <= max; j += 1) {
+          int k = sum - i - j;
+          if (k <= max && k >= 0) count += 1;
+        }
+      }
+      solution.append(count);
+    }
+		PW.print(solution.toString());
+    PW.close();
+  }
+ 
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+  
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+  
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException  e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+  
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+ 
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e)  {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
