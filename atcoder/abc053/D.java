d5d16f1a1191648017eae7eeb49d4e84b54e95ab
Warul Kumar Sinha <wksinha@outlook.com>
2021-01-22 18:32:00 +0530

    Add solution for problem `D - Card Eater` Link: https://atcoder.jp/contests/abc053/tasks/arc068_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc053/submissions/19566622

---
+++ atcoder/abc053/D.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+  static final FastReader FR = new FastReader();
+  static final PrintWriter PW = new PrintWriter(new OutputStreamWriter(System.out));
+ 
+  public static void main(String[] args) {
+    StringBuilder solution = new StringBuilder();
+    int tests = 1;
+    // tests = FR.nextInt();
+    for (int t = 0; t < tests; ++t) {
+      int cards = FR.nextInt();
+      Set<Integer> singles = new HashSet<Integer>();
+      int duplicateCount = 0;
+      for (int i = 0; i < cards; i += 1) {
+        int value = FR.nextInt();
+        if (singles.contains(value)) duplicateCount += 1;
+        else singles.add(value);
+      }
+      if ((duplicateCount&1) == 0) solution.append(singles.size() + "\n");
+      else solution.append(singles.size()-1 + "\n");
+    }
+		PW.print(solution.toString());
+    PW.close();
+  }
+ 
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+  
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+  
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException  e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+  
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+ 
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e)  {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
