1f038dd76b5ccfb0b783252189b8b1c99f0e84b5
Warul Kumar Sinha <wksinha@outlook.com>
2021-01-28 20:09:00 +0530

    Add solution for problem `A - Nezzar and Colorful Balls` Link: https://codeforces.com/contest/1478/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/1478/submission/105679565

---
+++ codeforces/1478/A.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+  static final FastReader FR = new FastReader();
+  static final PrintWriter PW = new PrintWriter(new OutputStreamWriter(System.out));
+ 
+  public static void main(String[] args) {
+    StringBuilder solution = new StringBuilder();
+    int tests = 1;
+    tests = FR.nextInt();
+    for (int t = 0; t < tests; ++t) {
+      int ballCount = FR.nextInt();
+      int freq, maxFreq, prev;
+      prev = freq = maxFreq = 0;
+      for (int i = 0; i < ballCount; i += 1) {
+        int val = FR.nextInt();
+        if (val == prev) freq += 1;
+        else freq = 1;
+        maxFreq = Math.max(freq, maxFreq);
+        prev = val;
+      }
+      solution.append(maxFreq + "\n");
+    }
+		PW.print(solution.toString());
+    PW.close();
+  }
+ 
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+  
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+  
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException  e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+  
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+ 
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e)  {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
