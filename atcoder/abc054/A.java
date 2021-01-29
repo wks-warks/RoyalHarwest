dd2e8985463cc27e80f7426a4858613e5d2258ce
Warul Kumar Sinha <wksinha@outlook.com>
2021-01-30 02:42:00 +0530

    Add solution for problem `A - One Card Poker` Link: https://atcoder.jp/contests/abc054/tasks/abc054_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc054/submissions/19760296

---
+++ atcoder/abc054/A.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+  static final FastReader FR = new FastReader();
+  static final PrintWriter PW = new PrintWriter(new OutputStreamWriter(System.out));
+ 
+  public static void main(String[] args) {
+    StringBuilder solution = new StringBuilder();
+    int tests = 1;
+    // tests = FR.nextInt();
+    for (int t = 0; t < tests; ++t) {
+      int aliceCard = FR.nextInt();
+      int bobCard = FR.nextInt();
+      String result = getResult(aliceCard, bobCard);
+      solution.append(result + "\n");
+    }
+		PW.print(solution.toString());
+    PW.close();
+  }
+
+  static String getResult(int aliceCard, int bobCard) {
+    if (aliceCard == 1) aliceCard += 13;
+    if (bobCard == 1) bobCard += 13;
+
+    return aliceCard > bobCard ? "Alice" : aliceCard == bobCard ? "Draw" : "Bob";
+  }
+ 
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+  
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+  
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException  e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+  
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+ 
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e)  {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
