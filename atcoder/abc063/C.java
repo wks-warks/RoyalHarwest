9c8066c38c1411d56dd78ef02f44cd4390f7c597
Warul Kumar Sinha <wksinha@outlook.com>
2021-02-22 17:24:00 +0530

    Add solution for problem `C - Bugged` Link: https://atcoder.jp/contests/abc063/tasks/arc075_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc063/submissions/20420992

---
+++ atcoder/abc063/C.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int questions = scanner.nextInt();
+    List<Integer> scores = new ArrayList<Integer>(questions);
+    for (int q = 0; q < questions; q += 1) {
+      scores.add(scanner.nextInt());
+    }
+    // int maxGrade = getMaxGrade(scores);
+    int maxGrade = efficientGetMaxGrade(scores);
+    System.out.println(maxGrade);
+  }
+
+  static int getMaxGrade(List<Integer> scores) {
+    int sum = 0;
+    PriorityQueue<Integer> notMultipleOf10 = new PriorityQueue<Integer>();
+    for (var score : scores) {
+      if (score % 10 != 0) {
+        notMultipleOf10.add(score);
+      }
+      sum += score;
+    }
+
+    if (sum % 10 != 0) {
+      return sum;
+    } else {
+      if (notMultipleOf10.size() > 0) {
+        return sum - notMultipleOf10.poll();
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  static int efficientGetMaxGrade(List<Integer> scores) {
+    int sum = 0;
+    int leastNonMultiple = Integer.MAX_VALUE;
+    for (var score : scores) {
+      sum += score;
+      if (score % 10 != 0 && score < leastNonMultiple) {
+        leastNonMultiple = score;
+      }
+    }
+
+    if (sum % 10 != 0) {
+      return sum;
+    } else {
+      if (leastNonMultiple != Integer.MAX_VALUE) {
+        return sum - leastNonMultiple;
+      } else {
+        return 0;
+      }
+    }
+  }
+}
