bd31e586f0f04a1130aa3505965cfbb17b3261a3
Warul Kumar Sinha <wksinha@outlook.com>
2021-02-24 16:44:00 +0530

    Add solution for problem `C - pushpush` Link: https://atcoder.jp/contests/abc066/tasks/arc077_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc066/submissions/20463219

--- atcoder/abc066/C.java
+++ atcoder/abc066/C.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.LinkedList;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int elementCount = scanner.nextInt();
-    LinkedList<Integer> elements = new LinkedList<Integer>();
+    Deque<Integer> elements = new ArrayDeque<Integer>(elementCount);
     for (int i = 0; i < elementCount; i += 1) {
       if ((i & 1) == 0) {
         elements.add(scanner.nextInt());
