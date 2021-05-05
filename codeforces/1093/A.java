71ffe936195df7545e84092e2e181f832557bae0
Warul Kumar Sinha <wksinha@outlook.com>
2021-05-05 13:44:00 +0530

    Add solution for problem `A - Dice Rolling` Link: https://codeforces.com/contest/1093/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1093/submission/115183500

---
+++ codeforces/1093/A.java
@@ -0,0 +1,68 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static final FastReader FR = new FastReader();
+  static final PrintWriter PW = new PrintWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) {
+    StringBuilder solution = new StringBuilder();
+    int tests = 1;
+    tests = FR.nextInt();
+    for (int t = 0; t < tests; ++t) {
+      int requiredSum = FR.nextInt();
+      int movesNeeded = getMovesNeeded(requiredSum);
+      solution.append(movesNeeded + "\n");
+    }
+		PW.print(solution.toString());
+    PW.close();
+  }
+
+  static int getMovesNeeded(int requiredSum) {
+    return requiredSum >> 1;
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException  e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e)  {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
