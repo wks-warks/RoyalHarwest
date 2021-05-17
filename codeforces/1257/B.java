5d92a3e3343891bb3b5b0ae39cb12d88adb43719
Warul Kumar Sinha <wksinha@outlook.com>
2021-05-17 09:41:00 +0530

    Add solution for problem `B - Magic Stick` Link: https://codeforces.com/contest/1257/problem/B Tags: math, *1000 Ref: https://codeforces.com/contest/1257/submission/116446366

---
+++ codeforces/1257/B.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.util.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static Output out = new Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), "RegalBeast", 1<<27).start();
+  }
+
+  public void run() {
+    StringBuilder sb = new StringBuilder();
+
+    int testCaseCount = in.nextInt();
+    for (int testCase = 1; testCase <= testCaseCount; testCase++) {
+      int currentNumber = in.nextInt();
+      int desiredNumber = in.nextInt();
+
+      boolean obtainable = isObtainable(currentNumber, desiredNumber);
+      // sb.append(obtainable ? "Yes\n" : "No\n");
+      out.println(obtainable ? "Yes" : "No");
+    }
+    
+    // out.print(sb.toString());
+    in.close();
+    out.close();
+  }
+
+  static boolean isObtainable(int currentNumber, int desiredNumber) {
+    return (currentNumber >= desiredNumber || currentNumber >= 4 || (currentNumber == 2 && desiredNumber == 3));
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
+
+class Output {
+  PrintWriter pw;
+  public Output() {
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+
+  public Output(String fileName) {
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void print(String s) {
+    pw.print(s);
+  }
+
+  public void println(String s) {
+    pw.println(s);
+  }
+
+  public void close() {
+    pw.close();
+  }
+}
