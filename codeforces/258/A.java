d73b45736f9b260bdb03093e85284cb30b273a44
Warul Kumar Sinha <wksinha@outlook.com>
2021-05-21 10:48:00 +0530

    Add solution for problem `A - Little Elephant and Bits` Link: https://codeforces.com/contest/258/problem/A Tags: greedy, math, *1100 Ref: https://codeforces.com/contest/258/submission/116879852

---
+++ codeforces/258/A.java
@@ -0,0 +1,137 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static Output out = new Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), "RegalBeast", 1<<27).start();
+  }
+
+  public void run() {
+    String binStr = in.next();
+    String answer = getAnswer(binStr);
+    out.println(answer);
+
+    in.close();
+    out.close();
+  }
+
+  static String getAnswer(String binStr) {
+    int zeroIdx = -1;
+    int oneIdx = -1;
+
+    for (int i = 0; i < binStr.length(); i++) {
+      if (binStr.charAt(i) == '1') {
+        oneIdx = i;
+      } else if (zeroIdx == -1) {
+        zeroIdx = i;
+      }
+    }
+
+    if (zeroIdx != -1) {
+      return binStr.substring(0, zeroIdx) + binStr.substring(zeroIdx+1);
+    } else {
+      return binStr.substring(0, oneIdx) + binStr.substring(oneIdx+1);
+    }
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
+
+class Output {
+  PrintWriter pw;
+  public Output() {
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+
+  public Output(String fileName) {
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void print(String s) {
+    pw.print(s);
+  }
+
+  public void println(String s) {
+    pw.println(s);
+  }
+
+  public void close() {
+    pw.close();
+  }
+}
