83a80fee7c180594bb315e1715b6eea05e1c5506
Warul Kumar Sinha <wksinha@outlook.com>
2021-05-23 01:37:00 +0530

    Add solution for problem `B - Rock and Lever` Link: https://codeforces.com/contest/1420/problem/B Tags: bitmasks, math, *1200 Ref: https://codeforces.com/contest/1420/submission/117046076

---
+++ codeforces/1420/B.java
@@ -0,0 +1,148 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static Output out = new Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), "RegalBeast", 1<<27).start();
+  }
+
+  public void run() {
+    StringBuilder sb = new StringBuilder();
+    int testCaseCount = in.nextInt();
+    for (int testCase = 1; testCase <= testCaseCount; testCase++) {
+      int arrayLength = in.nextInt();
+      int[] array = new int[arrayLength];
+
+      for (int i = 0; i < arrayLength; i++) {
+        array[i] = in.nextInt();
+      }
+
+      long answer = getAnswer(array);
+      sb.append(answer + "\n");
+    }
+
+    out.print(sb.toString());
+    in.close();
+    out.close();
+  }
+
+  static long getAnswer(int[] array) {
+    int[] msbCount = new int[30];
+    for (var num : array) {
+      for (int pow = 29; pow >= 0; pow--) {
+
+        if ((num & (1<<pow)) > 0) {
+          msbCount[pow]++;
+          break;
+        }
+      }
+    }
+
+    long answer = 0;
+    for (int pow = 29; pow >= 0; pow--) {
+      answer += (long) msbCount[pow] * (msbCount[pow] - 1) >> 1;
+    }
+    return answer;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
+
+class Output {
+  PrintWriter pw;
+  public Output() {
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+
+  public Output(String fileName) {
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void print(String s) {
+    pw.print(s);
+  }
+
+  public void println(String s) {
+    pw.println(s);
+  }
+
+  public void close() {
+    pw.close();
+  }
+}
