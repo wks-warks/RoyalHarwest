40ae61c36421fafc0fe9edc90a48d5fddc8f9f50
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-01 16:01:00 +0530

    Add solution for problem `A - Valera and X` Link: https://codeforces.com/contest/404/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/404/submission/118078507

---
+++ codeforces/404/A.java
@@ -0,0 +1,145 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static Output out = new Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), "RegalBeast", 1<<27).start();
+  }
+
+  public void run() {
+    int n = in.nextInt();
+    char[][] paper = new char[n][];
+    for (int r = 0; r < n; r++) {
+      paper[r] = in.next().toCharArray();
+    }
+
+    boolean xFormed = checkXFormed(paper);
+    out.println((xFormed ? "YES" : "NO"));
+    in.close();
+    out.close();
+  }
+
+  static boolean checkXFormed(char[][] paper) {
+    char first = paper[0][0];
+    char second = paper[0][1];
+    if (first == second) {
+      return false;
+    }
+
+    for (int r = 0; r < paper.length; r++) {
+      for (int c = 0; c < paper[r].length; c++) {
+        if (r == c || (paper.length - r - 1 == c)) {
+          if (paper[r][c] != first) {
+            return false;
+          }
+        } else {
+          if (paper[r][c] != second) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
+
+class Output {
+  PrintWriter pw;
+  public Output() {
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+
+  public Output(String fileName) {
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void print(String s) {
+    pw.print(s);
+  }
+
+  public void println(String s) {
+    pw.println(s);
+  }
+
+  public void close() {
+    pw.close();
+  }
+}
