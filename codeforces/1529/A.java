aea437d7aafffde4d82ca5bc6dc6b01de468911f
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-05 08:44:00 +0530

    Add solution for problem `A - Eshag Loves Big Arrays` Link: https://codeforces.com/contest/1529/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1529/submission/118466587

---
+++ codeforces/1529/A.java
@@ -0,0 +1,138 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int arrLen = in.nextInt();
+      int[] arr = new int[arrLen];
+      for (int i = 0; i < arrLen; i++) {
+        arr[i] = in.nextInt();
+      }
+
+      int maxDeletable = computeMaxDeletable(arr);
+      out.println(maxDeletable);
+    }
+
+    in.close();
+    out.close();
+  }
+
+  static int computeMaxDeletable(int[] arr) {
+    int min = Integer.MAX_VALUE;
+    int minCount = 1;
+    for (var num : arr) {
+      if (num == min) {
+        minCount++;
+      } else if (num < min) {
+        min = num;
+        minCount = 1;
+      }
+    }
+
+    return arr.length - minCount;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
