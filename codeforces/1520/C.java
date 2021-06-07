93304596a93bbdd790c1a928d909ea27441dd529
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-07 13:07:00 +0530

    Add solution for problem `C - Not Adjacent Matrix` Link: https://codeforces.com/contest/1520/problem/C Tags: constructive algorithms, *1000 Ref: https://codeforces.com/contest/1520/submission/118696671

---
+++ codeforces/1520/C.java
@@ -0,0 +1,149 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int n = in.nextInt();
+      int[][] matrix = getMatrix(n);
+
+      if (matrix == null) {
+        out.println(-1);
+      } else {
+        for (var row : matrix) {
+          for (var val : row) {
+            out.print(val + " ");
+          }
+          out.println();
+        }
+      }
+    }
+
+    in.close();
+    out.close();
+  }
+
+  static int[][] getMatrix(int n) {
+    if (n == 2) {
+      return null;
+    }
+    int max = n*n;
+
+    int num = 1;
+    int[][] matrix = new int[n][n];
+    for (int r = 0; r < n; r++) {
+      for (int c = 0; c < n; c++) {
+        matrix[r][c] = num;
+        if (num+1 >= max) {
+          num = 2;
+        } else {
+          num += 2;
+        }
+      }
+    }
+    return matrix;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
