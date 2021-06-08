08dd2fcc233d07b80a1ad955be61df2cc98e3500
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-08 12:44:00 +0530

    Add solution for problem `C - RationalLee` Link: https://codeforces.com/contest/1369/problem/C Tags: greedy, math, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/1369/submission/118779186

---
+++ codeforces/1369/C.java
@@ -0,0 +1,180 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int n = in.nextInt();
+      int friends = in.nextInt();
+
+      int[] integers = new int[n];
+      for (int i = 0; i < n; i++) {
+        integers[i] = in.nextInt();
+      }
+      
+      int[] integerDistribution = new int[friends];
+      for (int i = 0; i < friends; i++) {
+        integerDistribution[i] = in.nextInt();
+      }
+
+      long maxHappiness = getMaxHappiness(integers, integerDistribution);
+      out.println(maxHappiness);
+    }
+
+    in.close();
+    out.close();
+  }
+
+  static long getMaxHappiness(int[] integers, int[] integerDistribution) {
+    Sorter.sort(integerDistribution);
+    Sorter.sort(integers);
+  
+    long maxHappiness = 0;
+    int end = integers.length;
+    for (int f = 0; f < integerDistribution.length; f++) {
+      maxHappiness += integers[--end];
+    }
+
+    int singleEnd = integers.length;
+    for (var count : integerDistribution) {
+      if (count == 1) {
+        maxHappiness += integers[--singleEnd];
+      } else {
+        end = end - count + 1;
+        maxHappiness += integers[end];
+      }
+    }
+
+    return maxHappiness;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
+
+class Sorter {
+  static void sort(int[] arr) {
+    ThreadLocalRandom rand = ThreadLocalRandom.current();
+ 
+    for (int i = 0; i < arr.length-1; i++) {
+      int newPos = rand.nextInt(i, arr.length-1);
+      int temp = arr[i];
+      arr[i] = arr[newPos];
+      arr[newPos] = temp;
+    }
+ 
+    Arrays.sort(arr);
+  }
+
+  static void reverseSort(int[] arr) {
+    sort(arr);
+    int mid = arr.length >> 1;
+
+    for (int left = 0, right = arr.length-1; left < mid; left++, right--) {
+      int temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+    }
+  }
+}
