8682e8ffde3d5366c1fa8eac631aa32a580310ef
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-08 12:10:00 +0530

    Add solution for problem `B - Sifid and Strange Subsequences` Link: https://codeforces.com/contest/1529/problem/B Tags: greedy, math, sortings, *1100 Ref: https://codeforces.com/contest/1529/submission/118776716

---
+++ codeforces/1529/B.java
@@ -0,0 +1,169 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int n = in.nextInt();
+      int[] arr = new int[n];
+      for (int i = 0; i < n; i++) {
+        arr[i] = in.nextInt();
+      }
+
+      int longestStrange = getLongestStrange(arr);
+      out.println(longestStrange);
+    }
+
+    in.close();
+    out.close();
+  }
+
+  static int getLongestStrange(int[] arr) {
+    SorterA.sort(arr);
+
+    int longestStrange = 1;
+    int minDifference = Integer.MAX_VALUE;
+
+    for (int i = 1; i < arr.length; i++) {
+      minDifference = Math.min(minDifference, arr[i] - arr[i-1]);
+      if (arr[i] > 0 && arr[i] > minDifference) {
+        break;
+      }
+      longestStrange++;
+    }
+
+    return longestStrange;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class SorterA {
+  static void sort(int[] arr) {
+    ThreadLocalRandom rand = ThreadLocalRandom.current();
+
+    for (int i = 0; i < arr.length-1; i++) {
+      int newPos = rand.nextInt(i, arr.length-1);
+      int temp = arr[i];
+      arr[i] = arr[newPos];
+      arr[newPos] = temp;
+    }
+
+    Arrays.sort(arr);
+  }
+}
+
+
+// class SorterB {
+//   static void sort(int[] arr) {
+    // for (int i = 0; i < arr.length; i++) {
+    //   int r = i + (int) (Math.random() * (arr.length - i));
+    //   int temp = arr[i];
+    //   arr[i] = arr[r];
+    //   arr[r] = temp;
+    // }
+    // Arrays.sort(arr);
+//   }
+// }
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
