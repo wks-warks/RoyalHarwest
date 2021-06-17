4842c70796eeba13e0e2164b99026ec8168ba134
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-17 20:00:00 +0530

    Add solution for problem `A - Mike and palindrome` Link: https://codeforces.com/contest/798/problem/A Tags: brute force, constructive algorithms, strings, *1000 Ref: https://codeforces.com/contest/798/submission/119721456

---
+++ codeforces/798/A.java
@@ -0,0 +1,129 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    String input = in.next();
+    int differingPalindromicPairs = countDifferingPalindromicPairs(input);
+
+    boolean achievable = differingPalindromicPairs == 1 || (differingPalindromicPairs == 0 && ((input.length() & 1) == 1));
+    out.println(achievable ? "YES" : "NO");
+    in.close();
+    out.close();
+  }
+
+  static int countDifferingPalindromicPairs(String str) {
+    int mid = str.length()>>1;
+    int differingPalindromicPairs = 0;
+
+    for (int i = 0; i < mid; i++) {
+      if (str.charAt(i) != str.charAt(str.length()-1-i)) {
+        differingPalindromicPairs++;
+      }
+    }
+    return differingPalindromicPairs;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
