5d370a9bdee40d3acd99b0d073599eb90a10a9bd
Warul Kumar Sinha <wksinha@outlook.com>
2021-06-29 00:25:00 +0530

    Add solution for problem `A - Bulbs` Link: https://codeforces.com/contest/615/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/615/submission/120829091

---
+++ codeforces/615/A.java
@@ -0,0 +1,142 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int buttons = in.nextInt();
+    int bulbs = in.nextInt();
+    int[][] connections = new int[buttons][];
+    
+    for (int b = 0; b < buttons; b++) {
+      connections[b] = new int[in.nextInt()];
+      for (int c = 0; c < connections[b].length; c++) {
+        connections[b][c] = in.nextInt();
+      }
+    }
+
+    boolean brightChristmas = canLightAll(connections, bulbs);
+    out.println(brightChristmas ? "YES" : "NO");
+    in.close();
+    out.close();
+  }
+
+  static boolean canLightAll(int[][] connections, int bulbs) {
+    boolean[] canLight = new boolean[bulbs];
+    
+    for (int button = 0; button < connections.length; button++) {
+      for (int c = 0; c < connections[button].length; c++) {
+        canLight[connections[button][c]-1] = true;
+      }
+    }
+
+    for (var state : canLight) {
+      if (!state) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
