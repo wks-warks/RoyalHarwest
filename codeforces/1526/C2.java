b1e3fc13ad811124051bb22c3b8589f6d123ad20
Warul Kumar Sinha <wksinha@outlook.com>
2021-07-02 22:49:00 +0530

    Add solution for problem `C2 - Potions (Hard Version)` Link: https://codeforces.com/contest/1526/problem/C2 Tags: data structures, greedy, *1600 Ref: https://codeforces.com/contest/1526/submission/121132587

---
+++ codeforces/1526/C2.java
@@ -0,0 +1,137 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int potions = in.nextInt();
+    int[] boosts = new int[potions];
+    for (int p = 0; p < potions; p++) {
+      boosts[p] = in.nextInt();
+    }
+
+    int maxPotions = getMaxPotions(potions, boosts);
+    out.println(maxPotions);
+    in.close();
+    out.close();
+  }
+
+  static int getMaxPotions(int potions, int[] boosts) {
+    PriorityQueue<Integer> chosen = new PriorityQueue<Integer>();
+    long chosenSum = 0;
+
+    for (var boost : boosts) {
+      if (chosenSum + boost >= 0) {
+        chosenSum += boost;
+        chosen.add(boost);
+      } else if (chosen.size() > 0 && chosen.peek() < boost) {
+        chosenSum += boost - chosen.poll();
+        chosen.add(boost);
+      }
+    }
+
+    return chosen.size();    
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
