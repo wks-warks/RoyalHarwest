390782ee5081c907a906a562a808ba204f26bd2b
Warul Kumar Sinha <wksinha@outlook.com>
2021-07-02 22:03:00 +0530

    Add solution for problem `B - I Hate 1111` Link: https://codeforces.com/contest/1526/problem/B Tags: dp, math, number theory, *1400 Ref: https://codeforces.com/contest/1526/submission/121129458

---
+++ codeforces/1526/B.java
@@ -0,0 +1,140 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int num = in.nextInt();
+      boolean isRepresentable = checkRepresentable(num);
+      out.println(isRepresentable ? "yES" : "no");
+    }
+
+    in.close();
+    out.close();
+  }
+
+  static Map<Integer, Boolean> memo = new HashMap<Integer, Boolean>();
+  static boolean checkRepresentable(int num) {
+    if (num >= 1_110) {
+      return true;
+    }
+    if (num < 11) {
+      return false;
+    }
+    if (memo.containsKey(num)) {
+      return memo.get(num);
+    }
+    if (num % 11 == 0 || num % 111 == 0) {
+      memo.put(num, true);
+      return true;
+    }
+
+    boolean isRepresentable = checkRepresentable(num - 11) || checkRepresentable(num - 111);
+    memo.put(num, isRepresentable);
+    return isRepresentable;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
