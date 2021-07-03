c6a04bc8aa7f16721414d3f70ed09301ef7a607b
Warul Kumar Sinha <wksinha@outlook.com>
2021-07-03 11:50:00 +0530

    Add solution for problem `B - Valerii Against Everyone` Link: https://codeforces.com/contest/1438/problem/B Tags: constructive algorithms, data structures, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1438/submission/121161115

--- codeforces/1438/B.java
+++ codeforces/1438/B.java
@@ -1,131 +1,150 @@
-// Author : RegalBeast
-
-import java.io.*;
-import java.util.*;
-import java.util.concurrent.*;
-
-public class Main implements Runnable {
-  static Input in = new Input();
-  static PrintWriter out = Output();
-  
-  public static void main(String[] args) {
-    new Thread(null, new Main(), String.join(
-                                  "All that is gold does not glitter,",
-                                  "Not all those who wander are lost;",
-                                  "The old that is strong does not wither,",
-                                  "Deep roots are not reached by the frost.",
-                                  
-                                  "From the ashes a fire shall be woken,",
-                                  "A light from the shadows shall spring;",
-                                  "Renewed shall be blade that was broken,",
-                                  "The crownless again shall be king."
-                                ), 1<<25).start();
-  }
-
-  public void run() {
-    int tests = in.nextInt();
-    for (int t = 0; t < tests; t++) {
-      int n = in.nextInt();
-      boolean possible = false;
-      Set<Integer> seen = new HashSet<Integer>();
-      for (int i = 0; i < n; i++) {
-        int val = in.nextInt();
-
-        if (!possible) {
-          if (seen.contains(val)) {
-            possible = true;
-          } else {
-            seen.add(val);
-          }
+public class Main {
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private java.io.DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+ 
+        public Reader()
+        {
+            din = new java.io.DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
         }
+ 
+        public String readLine() throws java.io.IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+ 
+        public int nextInt() throws java.io.IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public long nextLong() throws java.io.IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        public double nextDouble() throws java.io.IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+ 
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+ 
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+ 
+            if (neg)
+                return -ret;
+            return ret;
+        }
+ 
+        private void fillBuffer() throws java.io.IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                 BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+ 
+        private byte read() throws java.io.IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+ 
+        public void close() throws java.io.IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+ 
+    public static void main(String[] args)
+        throws java.io.IOException
+    {
+        Reader s = new Reader();
+        java.io.PrintWriter pw = new java.io.PrintWriter(new java.io.BufferedWriter(new java.io.OutputStreamWriter(System.out)));
+        int tests = s.nextInt();
+        for (int t = 0; t < tests; t++)
+        {
+          int n = s.nextInt();
+          int[] b = new int[n];
+          for (int i = 0; i < n; i++)
+          {
+            b[i] = s.nextInt();
+          }
+          boolean possible = checkPossible(b);
+          pw.println(possible ? "yeS" : "nO");
+        }
+        pw.close();
+    }
+
+    static boolean checkPossible(int[] b)
+    {
+      java.util.Set<Integer> seen = new java.util.HashSet<Integer>();
+      for (var value : b)
+      {
+        if (seen.contains(value))
+        {
+          return true;
+        }
+        seen.add(value);
       }
-      out.println(possible ? "yES" : "NO");
+      return false;
     }
-
-    in.close();
-    out.close();
-  }
-
-  static PrintWriter Output() {
-    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-  }
-  
-  static PrintWriter Output(String fileName) {
-    PrintWriter pw = null;
-    try {
-      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
-    return pw;
-  }
-}
-
-class Input {
-  BufferedReader br;
-  StringTokenizer st;
-  public Input() {
-    br = new BufferedReader(new InputStreamReader(System.in));
-  }
-
-  public Input(String fileName) {
-    try {
-      br = new BufferedReader(new FileReader(fileName));
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
-  }
-
-  public String next() {
-    while (st == null || !st.hasMoreElements()) {
-      try {
-        st = new StringTokenizer(br.readLine());
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
-    }
-    return st.nextToken();
-  }
-
-  public int nextInt() {
-    return Integer.parseInt(next());
-  }
-
-  public long nextLong() {
-    return Long.parseLong(next());
-  }
-
-  public Float nextFloat() {
-    return Float.parseFloat(next());
-  }
-
-  public Double nextDouble() {
-    return Double.parseDouble(next());
-  }
-
-  public String nextLine() {
-    if (st != null && st.hasMoreElements()) {
-      StringBuilder sb = new StringBuilder();
-      while (st.hasMoreElements()) {
-        sb.append(next());
-      }
-      return sb.toString();
-    }
-
-    String str = null;
-    try {
-      str = br.readLine();
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
-    return str;
-  }
-
-  public void close() {
-    try {
-      br.close();
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
-  }
 }
