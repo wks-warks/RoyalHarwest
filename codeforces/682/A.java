a8128fb8e884ef09582ebee2f80c14340fdfa0f6
Warul Kumar Sinha <wksinha@outlook.com>
2021-07-07 16:49:00 +0530

    Add solution for problem `A - Alyona and Numbers` Link: https://codeforces.com/contest/682/problem/A Tags: constructive algorithms, math, number theory, *1100 Ref: https://codeforces.com/contest/682/submission/121556637

---
+++ codeforces/682/A.java
@@ -0,0 +1,145 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int n = in.nextInt();
+    int m = in.nextInt();
+    
+    long suitablePairs = countSuitablePairs(n, m);
+    out.println(suitablePairs);
+
+    in.close();
+    out.close();
+  }
+
+  static long countSuitablePairs(int n, int m) {
+    int[] nFreq = getFreq(n, 5);
+    int[] mFreq = getFreq(m, 5);
+    
+    long suitablePairs = 0;
+    for (int i = 0; i < 5; i++) {
+      suitablePairs += (long) nFreq[i] * mFreq[(5-i) % 5];
+    }
+    return suitablePairs;
+  }
+
+  static int[] getFreq(int num, int modVal) {
+    int[] freq = new int[modVal];
+    int each = num / modVal;
+    int remaining = num % modVal;
+
+    for (int i = 0; i < modVal; i++) {
+      freq[i] = each;
+    }
+    
+    int addTo = 1;
+    while (remaining-- > 0) {
+      freq[addTo++] += 1;
+    }
+    return freq;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
