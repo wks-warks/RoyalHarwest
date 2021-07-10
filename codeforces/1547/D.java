a3eb3380a45dcbd60291834d67e4dabdfe82f429
Warul Kumar Sinha <wksinha@outlook.com>
2021-07-10 20:59:00 +0530

    Add solution for problem `D - Co-growing Sequence` Link: https://codeforces.com/contest/1547/problem/D Tags: bitmasks, constructive algorithms, greedy, *1300 Ref: https://codeforces.com/contest/1547/submission/121956747

---
+++ codeforces/1547/D.java
@@ -0,0 +1,150 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int n = in.nextInt();
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+        x[i] = in.nextInt();
+      }
+
+      int[] y = getY(x);
+      for (var val : y) {
+        out.print(val + " ");
+      }
+      out.println();
+    }
+
+    in.close();
+    out.close();
+  }
+
+  static int[] getY(int[] x) {
+    int[] y = new int[x.length];
+    boolean[] prev = new boolean[30];
+
+    for (int i = 0; i < x.length; i++) {
+      int num = 0;
+      int bit = 1;
+      for (int j = 0; j < 30; j++, bit<<=1) {
+        if ((x[i] & bit) > 0) {
+          prev[j] = true;
+        } else {
+          if (prev[j]) {
+            num |= bit;
+          }
+        }
+      }
+      
+      y[i] = num;
+    }
+
+    return y;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
