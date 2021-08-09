373781d953a7c02fb3975650b32396fdd3d95665
Warul Kumar Sinha <wksinha@outlook.com>
2021-08-09 20:23:00 +0530

    Add solution for problem `B - Moamen and k-subarrays` Link: https://codeforces.com/contest/1557/problem/B Tags: greedy, sortings, *1100 Ref: https://codeforces.com/contest/1557/submission/125339756

---
+++ codeforces/1557/B.java
@@ -0,0 +1,135 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), "King", 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+
+      int n = in.nextInt();
+      int k = in.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < n; i++) {
+        a[i] = in.nextInt();
+      }
+
+      boolean possible = checkPossible(a, k);
+      out.println(possible ? "Yes" : "No");
+    }
+    
+    in.close();
+    out.close();
+  }
+
+  static boolean checkPossible(int[] a, int k) {
+    TreeSet<Integer> allValues = new TreeSet<Integer>();
+    for (var val : a) {
+      allValues.add(val);
+    }
+
+    int subarrays = 0;
+    int prev = Integer.MAX_VALUE;
+    for (var val : a) {
+      Integer successor = allValues.higher(prev);
+      if (successor == null || successor != val) {
+        subarrays++;
+      }
+      prev = val;
+    }
+
+    return subarrays <= k;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
