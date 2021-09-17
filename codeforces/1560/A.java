6228e3b0e78d4a1cf7c3ef9330932102d5e98eca
Warul Kumar Sinha <wksinha@outlook.com>
2021-09-17 18:24:00 +0530

    Add solution for problem `A - Dislike of Threes` Link: https://codeforces.com/contest/1560/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1560/submission/129067351

---
+++ codeforces/1560/A.java
@@ -0,0 +1,122 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), "King", 1<<25).start();
+  }
+
+  public void run() {
+    int[] liked = getLiked(1_000);
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      out.println(liked[in.nextInt()-1]);
+    }
+    in.close();
+    out.close();
+  }
+
+  static int[] getLiked(int upto) {
+    int[] liked = new int[upto];
+    int ptr = 0;
+    int num = 1;
+    
+    while (ptr < upto) {
+      if (num % 3 != 0 && num % 10 != 3) {
+        liked[ptr++] = num;
+      }
+      num++;
+    }
+
+    return liked;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
