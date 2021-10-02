f40d581754943f776a75d6d4dcd70b6fb8981f1c
Warul Kumar Sinha <wksinha@outlook.com>
2021-10-02 19:05:00 +0530

    Add solution for problem `B - typo` Link: https://atcoder.jp/contests/abc221/tasks/abc221_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc221/submissions/26314937

--- atcoder/abc221/B.java
+++ atcoder/abc221/B.java
@@ -1,179 +1,27 @@
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class Main implements Runnable {
-    static class FastReader {
-        final private int BUFFER_SIZE = 1 << 16;
-        private DataInputStream din;
-        private byte[] buffer;
-        private int bufferPointer, bytesRead;
-
-        public FastReader() {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public FastReader(String fileName) throws IOException {
-            din = new DataInputStream(new FileInputStream(fileName));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public String readLine() throws IOException {
-            byte[] buf = new byte[128];
-            int cnt = 0, ch;
-
-            while ((ch = read()) != -1) {
-                if (ch == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-
-                buf[cnt++] = (byte)ch;
-            }
-
-            return new String(buf, 0, cnt);
-        }
-
-        public int nextInt() throws IOException {
-            int val = 0;
-            byte ch = read();
-
-            while (ch <= ' ') {
-                ch = read();
-            }
-            
-            boolean negative = (ch == '-');
-            if (negative) {
-                ch = read();
-            }
-
-            do {
-                val = val * 10 + ch - '0';
-            } while ((ch = read()) >= '0' && ch <= '9');
-
-            if (negative) {
-                return -val;
-            } else {
-                return val;
-            }
-        }
-
-        public long nextLong() throws IOException {
-            long val = 0L;
-            byte ch = read();
-
-            while (ch <= ' ') {
-                ch = read();
-            }
-
-            boolean negative = (ch == '-');
-            if (negative) {
-                ch = read();
-            }
-
-            do {
-                val = val * 10 + ch - '0';
-            } while ((ch = read()) >= '0' && ch <= '9');
-
-            if (negative) {
-                return -val;
-            } else {
-                return val;
-            }
-        }
-
-        public double nextDouble() throws IOException {
-            double val = 0.0, div = 1.0;
-            byte ch = read();
-
-            while (ch <= ' ') {
-                ch = read();
-            }
-
-            boolean negative = (ch == '-');
-            if (negative) {
-                ch = read();
-            }
-
-            do {
-                val = val * 10.0 + ch - '0';
-            } while ((ch = read()) >= '0' && ch <= '9');
-
-            if (ch == '.') {
-                while ((ch = read()) >= '0' && ch <= '9') {
-                    val += (ch - '0') / (div *= 10.0);
-                }
-            }
-
-            if (negative) {
-                return -val;
-            } else {
-                return val;
-            }
-        }
-
-        private void fillBuffer() throws IOException {
-            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-            if (bytesRead == -1) {
-                buffer[0] = -1;
-            }
-        }
-
-        private byte read() throws IOException {
-            if (bufferPointer == bytesRead) {
-                fillBuffer();
-            }
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException {
-            if (din == null) {
-                return;
-            }
-            din.close();
-        }
-    }
-
-    static FastReader fr = new FastReader();
-    static PrintWriter pw = new PrintWriter(System.out);
+public class Main {
     public static void main(String[] args) {
-        new Thread(null, new Main(), "warks", 1<<28).start();
-    }
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        String t = scanner.next();
 
-    public void run() {
-        try {
-            String s = fr.readLine();
-            String t = fr.readLine();
+        boolean possible = s.equals(t);
 
-            boolean possible = s.equals(t);
+        for (int i = 0; i < s.length()-1; i++) {
+            StringBuilder sb = new StringBuilder(s);
+            char first = s.charAt(i);
+            char second = s.charAt(i+1);
 
-            for (int i = 0; i < s.length()-1; i++) {
-                StringBuilder sb = new StringBuilder(s);
-                char first = s.charAt(i);
-                char second = s.charAt(i+1);
+            sb.setCharAt(i, second);
+            sb.setCharAt(i+1, first);
 
-                sb.setCharAt(i, second);
-                sb.setCharAt(i+1, first);
-
-                if (sb.toString().equals(t)) {
-                    possible = true;
-                    break;
-                }
+            if (sb.toString().equals(t)) {
+                possible = true;
+                break;
             }
-
-            pw.println(possible ? "Yes" : "No");
-
-            fr.close();
-            pw.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
+
+        System.out.println(possible ? "Yes" : "No");
     }
 }
