242a794ac8816f0d6d57d02320554e58b0622088
Warul Kumar Sinha <wksinha@outlook.com>
2021-10-12 13:14:00 +0530

    Add solution for problem `C - Multiple Clocks` Link: https://atcoder.jp/contests/abc070/tasks/abc070_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc070/submissions/26519696

---
+++ atcoder/abc070/C.java
@@ -0,0 +1,142 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int clocks = in.nextInt();
+
+    long[] periods = new long[clocks];
+    for (int c = 0; c < clocks; c++) {
+      periods[c] = in.nextLong();
+    }
+
+    long lcm = getLCM(periods);
+    out.println(lcm);
+    
+    in.close();
+    out.close();
+  }
+
+  static long getLCM(long[] arr) {
+    long lcm = 1;
+    for (var num : arr) {
+      lcm = getLCM(lcm, num);
+    }
+    return lcm;
+  }
+
+  static long getLCM(long num1, long num2) {
+    long gcd = getGCD(num1, num2);
+    return num1 / gcd * num2;
+  }
+
+  static long getGCD(long num1, long num2) {
+    if (num1 == 0) {
+      return num2;
+    }
+    return getGCD(num2 % num1, num1);
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
