e9058d2bcbf2cdf388325bcd0cf1c90bc631bace
Warul Kumar Sinha <wksinha@outlook.com>
2021-10-13 20:28:00 +0530

    Add solution for problem `C - Save More Mice` Link: https://codeforces.com/contest/1593/problem/C Tags: greedy, greedy Ref: https://codeforces.com/contest/1593/submission/131772970

---
+++ codeforces/1593/C.java
@@ -0,0 +1,148 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int n = in.nextInt();
+      int k = in.nextInt();
+
+      int[] pos = new int[k];
+      for (int m = 0; m < k; m++) {
+        pos[m] = in.nextInt();
+      }
+
+      int saved = countSaved(pos, n);
+      out.println(saved);
+    }
+    
+    in.close();
+    out.close();
+  }
+
+  static int countSaved(int[] pos, int n) {
+    PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+    for (var point : pos) {
+      pq.add(point);
+    }
+
+    int cat = 0;
+    int saved = 0;
+    while (pq.size() > 0) {
+      int top = pq.poll();
+      
+      if (top <= cat) {
+        break;
+      } else {
+        saved++;
+        cat += n - top;
+      }
+    }
+
+    return saved;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
