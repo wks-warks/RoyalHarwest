fa49a0dcd178d34a1be7e13543de0c7ad41cbbf9
Warul Kumar Sinha <wksinha@outlook.com>
2021-10-13 20:09:00 +0530

    Add solution for problem `A - Elections` Link: https://codeforces.com/contest/1593/problem/A Tags: Ref: https://codeforces.com/contest/1593/submission/131751337

---
+++ codeforces/1593/A.java
@@ -0,0 +1,148 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int tests = in.nextInt();
+    for (int t = 0; t < tests; t++) {
+      int[] votes = new int[3];
+      for (int c = 0; c < 3; c++) {
+        votes[c] = in.nextInt();
+      }
+
+      int[] needed = getNeeded(votes);
+      for (int c = 0; c < 3; c++) {
+        out.print(needed[c] + " ");
+      }
+      out.println();
+    }
+    
+    in.close();
+    out.close();
+  }
+
+  static int[] getNeeded(int[] votes) {
+    int[] needed = new int[3];
+
+    for (int c1 = 0; c1 < 3; c1++) {
+
+      int opposition = Integer.MIN_VALUE;
+      for (int c2 = 0; c2 < 3; c2++) {
+        if (c1 == c2) {
+          continue;
+        }
+
+        opposition = Math.max(opposition, votes[c2]);
+      }
+
+      if (votes[c1] < opposition + 1) {
+        needed[c1] = opposition + 1 - votes[c1];
+      }
+    }
+
+    return needed;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
