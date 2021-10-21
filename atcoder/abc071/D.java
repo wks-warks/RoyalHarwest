17c67e9cde2482f79dc3c223493a2d61549a0bd8
Warul Kumar Sinha <wksinha@outlook.com>
2021-10-21 14:08:00 +0530

    Add solution for problem `D - Coloring Dominoes` Link: https://atcoder.jp/contests/abc071/tasks/arc081_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc071/submissions/26715770

---
+++ atcoder/abc071/D.java
@@ -0,0 +1,162 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int len = in.nextInt();
+    String s1 = in.next();
+    String s2 = in.next();
+
+    int ways = countWays(s1, s2, len);
+    out.println(ways);    
+    
+    in.close();
+    out.close();
+  }
+
+  static int countWays(String s1, String s2, int len) {
+    int mod = 1_000_000_007;
+    int state = s1.charAt(0) - s2.charAt(0);
+    
+    int ways = 0;
+    int idx = 0;
+    if (state == 0) {
+      ways = 3;
+      idx = 1;
+    } else {
+      ways = 6;
+      idx = 2;
+    }
+    
+    int prevState = state;
+    while (idx != len) {
+      state = s1.charAt(idx) - s2.charAt(idx);
+
+      if (prevState == 0) {
+        ways <<= 1;
+        ways %= mod;
+      } else if (state != 0) {
+        ways = (int) ((long) ways * 3 % mod);
+      }
+
+      prevState = state;
+      idx++;
+      if (state != 0) {
+        idx++;
+      }
+    }
+
+    return ways;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
+
+/*
+  Transitions, Colorings (constraint to left)
+    |=, 2 DONE
+    =|, 1
+    ||, 2 DONE
+    ==, 3 DONE
+*/
