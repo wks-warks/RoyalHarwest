8ccc5da664e6fde5a8053da7b1bd8fa3d1e703e0
Warul Kumar Sinha <wksinha@outlook.com>
2021-10-23 17:36:00 +0530

    Add solution for problem `B - Mongeness` Link: https://atcoder.jp/contests/abc224/tasks/abc224_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc224/submissions/26759083

---
+++ atcoder/abc224/B.java
@@ -0,0 +1,141 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int h = in.nextInt();
+    int w = in.nextInt();
+
+    int[][] grid = new int[h][w];
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j < w; j++) {
+        grid[i][j] = in.nextInt();
+      }
+    }
+    
+    boolean satisfied = isSatisfied(grid);
+    out.println(satisfied ? "Yes" : "No");
+
+    in.close();
+    out.close();
+  }
+
+  private static boolean isSatisfied(int[][] grid) {
+    boolean satisfied = true;
+
+    for (int i1 = 0; i1 < grid.length; i1++) {
+      for (int i2 = i1+1; i2 < grid.length; i2++) {
+        for (int j1 = 0; j1 < grid[i1].length; j1++) {
+          for (int j2 = j1+1; j2 < grid[i1].length; j2++) {
+            satisfied &= grid[i1][j1] + grid[i2][j2] <= grid[i2][j1] + grid[i1][j2];
+          }
+        }
+      }
+    }
+
+    return satisfied;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
