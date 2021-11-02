a543a8cccbcd802201fc47be8b28d453fc22db80
Warul Kumar Sinha <wksinha@outlook.com>
2021-11-02 10:21:00 +0530

    Add solution for problem `B - Substrings Sort` Link: https://codeforces.com/contest/988/problem/B Tags: sortings, strings, *1100 Ref: https://codeforces.com/contest/988/submission/134012406

---
+++ codeforces/988/B.java
@@ -0,0 +1,143 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<25).start();
+  }
+
+  public void run() {
+    int stringCount = in.nextInt();
+    String[] strings = new String[stringCount];
+    for (int c = 0; c < stringCount; c++) {
+      strings[c] = in.next();
+    }
+    Arrays.sort(strings, (a, b) -> a.length() - b.length());
+    
+    boolean ok = true;
+    for (int i = 1; i < stringCount; i++) {
+      boolean substring = isSubstring(strings[i], strings[i-1]);
+      ok &= substring;
+    }
+
+    if (!ok) {
+      out.println("NO");
+    } else {
+      out.println("YES");
+      for (var str : strings) {
+        out.println(str);
+      }
+    }
+
+    in.close();
+    out.close();
+  }
+  
+  private static boolean isSubstring(String str, String sub) {
+    for (int i = 0; i <= str.length() - sub.length(); i++) {
+      if (str.substring(i, i+sub.length()).equals(sub)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
