e3955af5989a642470be189df8eb4023e6da7bea
Warul Kumar Sinha <wksinha@outlook.com>
2021-11-25 14:32:00 +0530

    Add solution for problem `C - Takahashi's Information` Link: https://atcoder.jp/contests/abc088/tasks/abc088_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc088/submissions/27485171

---
+++ atcoder/abc088/C.java
@@ -0,0 +1,138 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.awt.Point;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<28).start();
+  }
+
+  public void run() {
+    int[][] c = new int[3][3];
+    for (int i = 0; i < c.length; i++) {
+      for (int j = 0; j < c[i].length; j++) {
+        c[i][j] = in.nextInt();
+      }
+    }
+
+    boolean possible = isPossible(c);
+    out.println(possible ? "Yes" : "No");
+    
+    in.close();
+    out.close();
+  }
+
+  private static boolean isPossible(int[][] c) {
+    int[] b = new int[] {c[0][0], c[0][1], c[0][2]};
+    int[] a = new int[] {c[0][0] - b[0], c[1][0] - b[0], c[2][0] - b[0]};
+
+    for (int i = 0; i < c.length; i++) {
+      for (int j = 0; j < c[i].length; j++) {
+        if (c[i][j] != a[i] + b[j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
