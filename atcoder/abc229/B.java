9f754f3fcb7647ee734abbdd56f8be5d3cfc182b
Warul Kumar Sinha <wksinha@outlook.com>
2021-11-27 17:37:00 +0530

    Add solution for problem `B - Hard Calculation` Link: https://atcoder.jp/contests/abc229/tasks/abc229_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc229/submissions/27518739

---
+++ atcoder/abc229/B.java
@@ -0,0 +1,138 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.awt.Point;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<28).start();
+  }
+
+  public void run() {
+    long a = in.nextLong();
+    long b = in.nextLong();
+
+    boolean noCarry = checkNoCarry(a, b);
+    out.println(noCarry ? "Easy" : "Hard");
+
+    in.close();
+    out.close();
+  }
+
+  private static boolean checkNoCarry(long a, long b) {
+    long min = Math.min(a, b);
+    long max = a + b - min;
+
+    while (min > 0) {
+      int minDigit = (int) (min % 10);
+      int maxDigit = (int) (max % 10);
+
+      if (minDigit + maxDigit > 9) {
+        return false;
+      }
+
+      min /= 10;
+      max /= 10;
+    }
+
+    return true;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
