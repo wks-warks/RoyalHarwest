3f2d9591f8901a5f9a8737cefa2736be76837db3
Warul Kumar Sinha <wksinha@outlook.com>
2021-12-02 13:22:00 +0530

    Add solution for problem `D - Binomial Coefficients` Link: https://atcoder.jp/contests/abc094/tasks/arc095_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc094/submissions/27626528

--- README.md
+++ README.md
@@ -13,14 +13,15 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1109 | [E - Fraction Floor Sum](https://atcoder.jp/contests/abc230/tasks/abc230_e) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/E.java) | `AtCoder` `*500` | Dec/05/2021 15:42 | 
-1108 | [D - Destroyer Takahashi](https://atcoder.jp/contests/abc230/tasks/abc230_d) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/D.java) | `AtCoder` `*400` | Dec/05/2021 15:24 | 
-1107 | [C - X drawing](https://atcoder.jp/contests/abc230/tasks/abc230_c) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/C.java) | `AtCoder` `*300` | Dec/05/2021 15:16 | 
-1106 | [B - Triple Metre](https://atcoder.jp/contests/abc230/tasks/abc230_b) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/B.java) | `AtCoder` `*200` | Dec/05/2021 15:03 | 
-1105 | [A - AtCoder Quiz 3](https://atcoder.jp/contests/abc230/tasks/abc230_a) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/A.java) | `AtCoder` `*100` | Dec/05/2021 14:59 | 
-1104 | [A - Cats and Dogs](https://atcoder.jp/contests/abc094/tasks/abc094_a) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/A.java) | `AtCoder` `*100` | Dec/02/2021 13:47 | 
-1103 | [B - Toll Gates](https://atcoder.jp/contests/abc094/tasks/abc094_b) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/B.java) | `AtCoder` `*200` | Dec/02/2021 13:45 | 
-1102 | [C - Many Medians](https://atcoder.jp/contests/abc094/tasks/arc095_a) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/C.java) | `AtCoder` `*300` | Dec/02/2021 13:39 | 
+1110 | [E - Fraction Floor Sum](https://atcoder.jp/contests/abc230/tasks/abc230_e) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/E.java) | `AtCoder` `*500` | Dec/05/2021 15:42 | 
+1109 | [D - Destroyer Takahashi](https://atcoder.jp/contests/abc230/tasks/abc230_d) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/D.java) | `AtCoder` `*400` | Dec/05/2021 15:24 | 
+1108 | [C - X drawing](https://atcoder.jp/contests/abc230/tasks/abc230_c) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/C.java) | `AtCoder` `*300` | Dec/05/2021 15:16 | 
+1107 | [B - Triple Metre](https://atcoder.jp/contests/abc230/tasks/abc230_b) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/B.java) | `AtCoder` `*200` | Dec/05/2021 15:03 | 
+1106 | [A - AtCoder Quiz 3](https://atcoder.jp/contests/abc230/tasks/abc230_a) | [Java (OpenJDK 11.0.6)](./atcoder/abc230/A.java) | `AtCoder` `*100` | Dec/05/2021 14:59 | 
+1105 | [A - Cats and Dogs](https://atcoder.jp/contests/abc094/tasks/abc094_a) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/A.java) | `AtCoder` `*100` | Dec/02/2021 13:47 | 
+1104 | [B - Toll Gates](https://atcoder.jp/contests/abc094/tasks/abc094_b) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/B.java) | `AtCoder` `*200` | Dec/02/2021 13:45 | 
+1103 | [C - Many Medians](https://atcoder.jp/contests/abc094/tasks/arc095_a) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/C.java) | `AtCoder` `*300` | Dec/02/2021 13:39 | 
+1102 | [D - Binomial Coefficients](https://atcoder.jp/contests/abc094/tasks/arc095_b) | [Java (OpenJDK 11.0.6)](./atcoder/abc094/D.java) | `AtCoder` `*400` | Dec/02/2021 13:22 | 
 1101 | [D - Longest X](https://atcoder.jp/contests/abc229/tasks/abc229_d) | [Java (OpenJDK 11.0.6)](./atcoder/abc229/D.java) | `AtCoder` `*400` | Nov/27/2021 17:51 | 
 1100 | [C - Cheese](https://atcoder.jp/contests/abc229/tasks/abc229_c) | [Java (OpenJDK 11.0.6)](./atcoder/abc229/C.java) | `AtCoder` `*300` | Nov/27/2021 17:43 | 
 1099 | [B - Hard Calculation](https://atcoder.jp/contests/abc229/tasks/abc229_b) | [Java (OpenJDK 11.0.6)](./atcoder/abc229/B.java) | `AtCoder` `*200` | Nov/27/2021 17:37 | 
---
+++ atcoder/abc094/D.java
@@ -0,0 +1,154 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.awt.Point;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<28).start();
+  }
+
+  public void run() {
+    int n = in.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = in.nextInt();
+    }
+
+    Point ans = getAns(a);
+    out.println(ans.x + " " + ans.y);
+    
+    in.close();
+    out.close();
+  }
+
+  private static Point getAns(int[] a) {
+    TreeSet<Integer> ts = new TreeSet<Integer>();
+    for (var num : a) {
+      ts.add(num);
+    }
+
+    Point ans = new Point(-1, -1);
+    ans.x = ts.last();
+    ts.remove(ans.x);
+
+    Integer ceil = ts.ceiling(ans.x>>1);
+    Integer floor = ts.floor(ans.x>>1);
+
+    ans.y = getY(ans.x, ceil, floor);
+    return ans;
+  }
+
+  private static int getY(int n, Integer ceil, Integer floor) {
+    if (ceil == null) {
+      return floor;
+    } else if (floor == null) {
+      return ceil;
+    }
+    int ceilDist = getDist(ceil, n);
+    int floorDist = getDist(floor, n);
+    return ceilDist > floorDist ? ceil : floor;
+  }
+
+  private static int getDist(int r, int n) {
+    return Math.min(r, n-r+1);
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
