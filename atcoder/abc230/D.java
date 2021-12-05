223980880cda53a61eac222d8ec3e9d99b40680e
Warul Kumar Sinha <wksinha@outlook.com>
2021-12-05 15:24:00 +0530

    Add solution for problem `D - Destroyer Takahashi` Link: https://atcoder.jp/contests/abc230/tasks/abc230_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc230/submissions/27709655

---
+++ atcoder/abc230/D.java
@@ -0,0 +1,139 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.awt.Point;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<28).start();
+  }
+
+  public void run() {
+    int n = in.nextInt();
+    int d = in.nextInt();
+    Integer[][] walls = new Integer[n][2];
+    for (int i = 0; i < n; i++) {
+      walls[i][0] = in.nextInt();
+      walls[i][1] = in.nextInt();
+    }
+
+    int punches = countPunches(walls, d);
+    out.println(punches);
+
+    in.close();
+    out.close();
+  }
+
+  private static int countPunches(Integer[][] walls, int d) {
+    Arrays.sort(walls, (a, b) -> a[1] - b[1]);
+    int punches = 0;
+    int destroyed = -1;
+
+    for (var wall : walls) {
+      if (wall[0] > destroyed) {
+        destroyed = wall[1] + d - 1;
+        punches++;
+      }
+    }
+
+    return punches;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
