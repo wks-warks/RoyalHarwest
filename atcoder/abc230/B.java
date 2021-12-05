db1645c08cd30f117878caf52653b187604a23ef
Warul Kumar Sinha <wksinha@outlook.com>
2021-12-05 15:03:00 +0530

    Add solution for problem `B - Triple Metre` Link: https://atcoder.jp/contests/abc230/tasks/abc230_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc230/submissions/27709344

---
+++ atcoder/abc230/B.java
@@ -0,0 +1,134 @@
+// Author : RegalBeast
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.awt.Point;
+
+public class Main implements Runnable {
+  static Input in = new Input();
+  static PrintWriter out = Output();
+  
+  public static void main(String[] args) {
+    new Thread(null, new Main(), String.join(
+                                  "All that is gold does not glitter,",
+                                  "Not all those who wander are lost;",
+                                  "The old that is strong does not wither,",
+                                  "Deep roots are not reached by the frost.",
+                                  
+                                  "From the ashes a fire shall be woken,",
+                                  "A light from the shadows shall spring;",
+                                  "Renewed shall be blade that was broken,",
+                                  "The crownless again shall be king."
+                                ), 1<<28).start();
+  }
+
+  public void run() {
+    String t = getT("oxx", 4);
+    String s = in.next();
+    boolean isSubstring = checkSubstring(t, s);
+    out.println(isSubstring ? "Yes" : "No");
+    
+    in.close();
+    out.close();
+  }
+
+  private static String getT(String rep, int count) {
+    if (count == 0) {
+      return "";
+    }
+    return rep + getT(rep, count-1);
+  }
+
+  private static boolean checkSubstring(String t, String s) {
+    for (int i = 0; i + s.length() <= t.length(); i++) {
+      if (t.substring(i, i+s.length()).equals(s)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  static PrintWriter Output() {
+    return new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  }
+  
+  static PrintWriter Output(String fileName) {
+    PrintWriter pw = null;
+    try {
+      pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return pw;
+  }
+}
+
+class Input {
+  BufferedReader br;
+  StringTokenizer st;
+  public Input() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+  public Input(String fileName) {
+    try {
+      br = new BufferedReader(new FileReader(fileName));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public String next() {
+    while (st == null || !st.hasMoreElements()) {
+      try {
+        st = new StringTokenizer(br.readLine());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return st.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+
+  public long nextLong() {
+    return Long.parseLong(next());
+  }
+
+  public Float nextFloat() {
+    return Float.parseFloat(next());
+  }
+
+  public Double nextDouble() {
+    return Double.parseDouble(next());
+  }
+
+  public String nextLine() {
+    if (st != null && st.hasMoreElements()) {
+      StringBuilder sb = new StringBuilder();
+      while (st.hasMoreElements()) {
+        sb.append(next());
+      }
+      return sb.toString();
+    }
+
+    String str = null;
+    try {
+      str = br.readLine();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return str;
+  }
+
+  public void close() {
+    try {
+      br.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
